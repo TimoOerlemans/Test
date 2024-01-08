cd0d07574a2fe7d0cfe406f4d95dbe3858a32b5c
Oerlemans,Timo <t.oerlemans@student.fontys.nl>
2024-01-08 13:50:31 +0100

    Update Where_to_go_Timo.py

--- pages/Where_to_go_Timo.py
+++ pages/Where_to_go_Timo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import folium
-from streamlit_folium import folium_static
 import json
+from streamlit_folium import folium_static
 
 data = pd.read_csv('./data/Timo_Where_to_go.csv', sep=';')
 
@@ -16,19 +16,25 @@ selected_phase = st.sidebar.selectbox('Selecteer projectfase', data['PROJECTFASE
 filtered_data = data[data['PROJECTFASE'] == selected_phase]
 
 # Itereren over de dataset om gebieden toe te voegen als rode polygoon op de kaart
-for index, row in data.iterrows():
-    geo_shape = json.loads(row['geo_shape'])
-    coordinates = geo_shape['coordinates']
+for index, row in filtered_data.iterrows():
+    try:
+        geo_shape = json.loads(row['geo_shape'])
+        coordinates = geo_shape['coordinates']  # Coördinaten voor het polygoon van elk project
 
-    # Polygoon toevoegen aan de kaart als een rode zone met projectnaam als popup
-    folium.Polygon(
-        locations=coordinates[0],
-        color='red',
-        fill=True,
-        fill_color='red',
-        fill_opacity=1,
-        popup=row['NAAMPROJECT']
-    ).add_to(m)
+        # Controleer de coördinaten die je ophaalt
+        st.write(f"Coördinaten voor {row['NAAMPROJECT']}:", coordinates)
+
+        # Polygoon toevoegen aan de kaart als een rode zone met projectnaam als popup
+        folium.Polygon(
+            locations=coordinates[0],  # De coördinaten van het polygon
+            color='red',
+            fill=True,
+            fill_color='red',
+            fill_opacity=0.4,
+            popup=row['NAAMPROJECT']
+        ).add_to(m)
+    except Exception as e:
+        st.write(f"Fout bij het toevoegen van polygoon voor {row['NAAMPROJECT']}: {e}")
 
 # Weergeven van de kaart in Streamlit
 st.header("Kaart van Eindhoven met rode gebieden voor elk project")
