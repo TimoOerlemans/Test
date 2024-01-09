65b910395f8f50a3508dbae24b80e641dc53d807
Oerlemans,Timo <t.oerlemans@student.fontys.nl>
2024-01-09 13:57:55 +0100

    Update Where_to_go_Timo.py

--- pages/Where_to_go_Timo.py
+++ pages/Where_to_go_Timo.py
@@ -5,13 +5,22 @@ from streamlit_folium import folium_static
 
 data = pd.read_csv('./data/Timo_Where_to_go.csv', sep=';')
 
+# Drop rows with NaN values in 'PRIJSKLASSE', 'PROJECTFASE', and 'WONINGTYPE' columns
+columns_to_check = ['PRIJSKLASSE', 'PROJECTFASE', 'WONINGTYPE']
+data = data.dropna(subset=columns_to_check)
+
+
 # Show unique values of PRIJSKLASSE
 unique_prices = data['PRIJSKLASSE'].unique()
 price_checkboxes = {price: st.sidebar.checkbox(f"Toon '{price}' projects", value=True) for price in unique_prices}
 
 # Show unique values of PROJECTFASE
 unique_phases = data['PROJECTFASE'].unique()
-phase_checkboxes = {phase: st.sidebar.checkbox(f"Toon '{phase}' projects", value=True) for phase in unique_phases}
+phase_checkboxes = {phase: st.sidebar.checkbox(f"'{phase}'", value=True) for phase in unique_phases}
+
+# Show unique values of PROJECTFASE
+unique_type = data['WONINGTYPE'].unique()
+type_checkboxes = {type: st.sidebar.checkbox(f"Toon '{type}'", value=True) for type in unique_type}
 
 # Show unique naamprojecten and their coordinates at the top
 unique_projects = data[['NAAMPROJECT', 'geo_point_2d']].drop_duplicates()
@@ -29,6 +38,11 @@ def update_displayed_projects():
             filtered_data = filtered_data[filtered_data['PRIJSKLASSE'] != price]
     return filtered_data
 
+    for type, checkbox in type_checkboxes.items():
+        if not checkbox:
+            filtered_data = filtered_data[filtered_data['WONINGTYPE'] != type]
+    return filtered_data
+
 # Kaart van Eindhoven initialiseren
 m = folium.Map(location=[51.4416, 5.4697], zoom_start=12)
 
@@ -36,7 +50,7 @@ m = folium.Map(location=[51.4416, 5.4697], zoom_start=12)
 def update_project_list():
     displayed_projects = update_displayed_projects()
     project_list.markdown("### Unieke Naamprojecten en Coördinaten")
-    project_list.write(displayed_projects[['NAAMPROJECT', 'PRIJSKLASSE', 'WONINGTYPE', ]].drop_duplicates())
+    project_list.write(displayed_projects[['NAAMPROJECT', 'PRIJSKLASSE', 'WONINGTYPE', 'PROJECTFASE']].drop_duplicates())
 
 # Update displayed projects and list
 update_project_list()
