7966fb37a0428d43cd0b0dbbc4c6a462fece4d97
Oerlemans,Timo <t.oerlemans@student.fontys.nl>
2024-01-10 11:34:31 +0100

    Update Where_to_go_Timo.py

--- pages/Where_to_go_Timo.py
+++ pages/Where_to_go_Timo.py
@@ -47,7 +47,7 @@ for index, row in filtered_data.iterrows():
     folium.Marker(
         location=[latitude, longitude],
         popup=row['NAAMPROJECT'],
-        icon=folium.Icon(color='lightred', icon='circle', opacity=0.4)
+        icon=folium.Icon(color='lightred', icon='circle', opacity=0.1)
     ).add_to(m)
 
 st.header("Kaart van Eindhoven met rode gebieden voor elk project")
